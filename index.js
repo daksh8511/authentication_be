77ad223aed2054607fab18760cd68bc906d7e390
Daksh03 <daxsathwara102@gmail.com>
2025-12-15 17:08:51 +0530

    cors add

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import ConnectDB from './config/ConnectDB.js'
 import routes from './routes/UserRoutes.js'
+import cors from 'cors'
 
 dotenv.config()
 ConnectDB()
@@ -10,6 +11,11 @@ const PORT = process.env.PORT || 3000
 const app = express();
 
 app.use(express.json())
+app.use(cors({
+    origin : ['http://localhost:4000', 'https://authentication-be-gooz.onrender.com']
+}))
+
+
 
 app.use('/api', routes)
 
